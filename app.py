46d0b341211522564d9d4fc2597c85b13af4823f
José Carlos <josecarlosnascimentosantos007@gmail.com>
2025-08-13 21:43:39 -0300

    Alterações finais no css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 import sqlite3
